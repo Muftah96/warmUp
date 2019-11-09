77a848db19e3956e5c2ead243b5c39ee2d1e5e48
muftah <muftah.elzawi@gmail.com>
2019-11-09 10:42:02 +0200

    more wormup

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,31 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+
+// i couldn't think in straight way my mind keeps distracting me and worrying about the project. 
+
+// all what i have now is that we need to split the text and loop over the array
+//  but i have no idea now for how to comper it to every other element in the array 
+// and make a counter for evey index that will incremint if we find the same char
+
+function symbol(argument) {
+	// body...
+	var arr = argument.split('')
+	var array = [];
+	
+	for (var i = 0; i < arr.length; i++) {
+		array = arr[i].slice(i,1,1)
+	}
+	
+
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = i; j < array.length; j++) {
+			if (arr[i] === arr[j]){
+				array[i] = Number.array[i] + 1
+
+			}
+		}
+	}
+	return array
+}
